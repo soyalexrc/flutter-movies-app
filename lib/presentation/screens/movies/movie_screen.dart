06da1e6ad1a9967e7502451a74570781aa1e59b5
alexrodriguez <alexcarvajal2404@gmail.com>
2023-07-27 16:52:34 -0500

    :tada: actors by movie module

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_by_movie_provider.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,6 +22,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
 
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
@@ -39,10 +42,154 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     return Scaffold(
       body: CustomScrollView(
           physics: const ClampingScrollPhysics(),
-          slivers: [_CustomSliverAppBar(movie: movie)]),
+          slivers: [
+            _CustomSliverAppBar(movie: movie),
+
+            SliverList(delegate: SliverChildBuilderDelegate(
+              childCount: 1,
+                (context, index) => _MovieDetails(movie: movie)
+            ))
+          ]),
     );
   }
 }
+
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch( actorsByMovieProvider );
+
+    if ( actorsByMovie[movieId] == null ) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                // Actor Photo
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                // Nombre
+                const SizedBox(height: 5,),
+
+                Text(actor.name, maxLines: 2 ),
+                Text(actor.character ?? '',
+                  maxLines: 2,
+                  style: const TextStyle( fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis ),
+                ),
+
+              ],
+            ),
+          );
+
+
+        },
+      ),
+    );
+
+  }
+}
+
+
+
+class _MovieDetails extends StatelessWidget {
+
+  final Movie movie;
+
+  const _MovieDetails({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                      movie.posterPath,
+                    width: size.width * 0.3,
+                  ),
+                ),
+                const SizedBox(width: 10),
+
+                SizedBox(
+                  width: (size.width - 40) * 0.7,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(movie.title, style: textStyles.titleLarge),
+                      Text(movie.overview)
+                    ],
+                  )
+                )
+
+              ],
+          )
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: Chip(
+                  label: Text(gender),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  )
+                ),
+              ))
+            ],
+          ),
+        ),
+
+        _ActorsByMovie(movieId: movie.id.toString()),
+
+        const SizedBox(height: 50,)
+      ],
+    );
+  }
+}
+
 
 class _CustomSliverAppBar extends StatelessWidget {
   final Movie movie;
@@ -59,17 +206,21 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          title: Text(
-        movie.title,
-        style: const TextStyle(fontSize: 20),
-        textAlign: TextAlign.start,
-      ),
+      //     title: Text(
+      //   movie.title,
+      //   style: const TextStyle(fontSize: 20),
+      //   textAlign: TextAlign.start,
+      // ),
         background: Stack(
           children: [
             SizedBox.expand(
                 child: Image.network(
                   movie.posterPath,
-                  fit: BoxFit.cover
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress != null) return const SizedBox();
+                      return FadeIn(child: child);
+                  },
                 )
             ),
 
