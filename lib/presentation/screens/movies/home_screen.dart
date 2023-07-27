78fb071aa9d5b85cb5a0a9dae2694eb08d039604
alexrodriguez <alexcarvajal2404@gmail.com>
2023-07-26 21:41:18 -0500

    :tada: initial commit styles and router initialization

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static const name = 'home-screen';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
